0152ffefd4268494f495fcae25f2911632723c66
Leonardo <rufinodevelopment@gmail.com>
2024-06-05 12:28:55 -0300

    Creating post-list component and styling it

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -3,23 +3,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { PostList } from './posts/post-list/post-list.component';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent,
+    PostList
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
     FormsModule,
-    MatInputModule, MatCardModule, MatButtonModule
+    MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule
   ],
   providers: [
     provideAnimationsAsync()
